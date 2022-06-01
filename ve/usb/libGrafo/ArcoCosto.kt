67600b91879eaced94ffc6f765bcb705c92a98c7
A6575 <astridaalvaradoa@gmail.com>
2022-06-01 01:34:46 -0400

    implementación ArcoCosto

--- ve/usb/libGrafo/ArcoCosto.kt
+++ ve/usb/libGrafo/ArcoCosto.kt
@@ -9,6 +9,6 @@ public class ArcoCosto(val x: Int, val y: Int, val costo: Double) : Arco(x, y) {
 
     // Representación del arco como String
     override fun toString() : String {
-
+        return "$x -> $y"
     }
 } 
