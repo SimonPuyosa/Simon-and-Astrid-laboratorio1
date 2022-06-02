7271ce688d8eef06f487f88c5e6e3c365cd92903
A6575 <astridaalvaradoa@gmail.com>
2022-06-02 18:56:27 -0400

    Falta por terminar

--- ve/usb/libGrafo/GrafoDirigidoCosto.kt
+++ ve/usb/libGrafo/GrafoDirigidoCosto.kt
@@ -1,11 +1,24 @@
 package ve.usb.libGrafo
 
 import java.io.File
+import java.util.*
+import javax.management.openmbean.KeyAlreadyExistsException
 
 public class GrafoDirigidoCosto : Grafo {
+    // Propiedades del grafo dirigido
+    var listaDeAdyacencia: Array<LinkedList<Vertice>?> = arrayOf(LinkedList())       // Arreglo, que puede ser nulo, de elementos que puden ser listas enlazadas o nulos
+    var numDeLados: Int = 0
+    lateinit var listaDeVertices: Array<Vertice?>                 // arreglo que contiene todos los vertices sin repeticiones
+    var numDeVertices = 0
 
     // Se construye un grafo a partir del número de vértices
     constructor(numDeVertices: Int) {
+        /** Entrada: un entero que determina el numero de vertices que tendra el grafo
+         *  Precondicion: numDeVertices > 0
+         *  Postcondicion: listaDeAdyacencia.size() == numDeVertices
+         *  Tiempo: de operacion: O(|V|)
+         */
+        listaDeAdyacencia = arrayOfNulls(numDeVertices)
     }
 
     /*
@@ -17,6 +30,57 @@ public class GrafoDirigidoCosto : Grafo {
      Se asume que los datos del archivo están correctos, no se verifican.
      */  
     constructor(nombreArchivo: String)  {
+        /** Entrada: un string de la ubicacion del archivo a buscar
+         *  Precondicion: el string debe tener una ubicacion real y valida del archivo
+         *  Postcondicion:
+         *      numDeVertice = Archivo[0]
+         *      numDeLados = Archivo[1]
+         *      listaDeAdyacencia.sizes == listaDeVertices.size == numDeVertices
+         *  Tiempo de operacion: O(|V| + |E|)
+         */
+        val a = File(nombreArchivo).readLines()                 // Se guarda el archivo en la variable a de tipo List<String>
+        numDeVertices = a[0].toInt()                            // Se obtiene de la primera linea linea el numero de vertices
+        listaDeAdyacencia = arrayOfNulls(numDeVertices)         // Se genera la lista de adyacencia (array de listas enlazadas)
+        listaDeVertices = arrayOfNulls(numDeVertices)           // Se genera la lista de vertices la cual sera una lista enlazada
+        numDeLados = a[1].toInt()                               // Se obtiene de la segunda linea el numero de lados
+
+        // Agregamos los lados a la lista de adyacencia
+        var i = 2
+        var v: Int
+        var u: Int
+        var temp: List<String>
+        var vertice1: Vertice
+        var vertice2: Vertice
+        var j: Int
+
+        while (i < 2 + numDeLados && a[i] != ""){                // se itera por las demas lineas del archivo hasta que este se acabe
+            temp = a[i].split(" ").filter {it != ""}   // se separa cada linea por espacios
+
+            if (listaDeAdyacencia[temp[0].toInt()] == null){     // si el elemento del arreglo es nulo se le asigna un linked list vacio
+                listaDeAdyacencia[temp[0].toInt()] = LinkedList<Vertice>()
+            }
+
+            v = temp[1].toInt()
+            u = temp[0].toInt()
+            vertice2 = Vertice(v)
+            vertice1 = Vertice(u)
+
+            if (listaDeAdyacencia[u]!!.indexOf(vertice1) != -1) throw KeyAlreadyExistsException("el objeto esta repetido")
+            listaDeAdyacencia[temp[u].toInt()]!!.addFirst(vertice2)       // se agrega el vertice 2, al inicio del arreglo
+            listaDeAdyacencia[temp[u].toInt()]!![0].Costo = temp[2].toDouble()
+            if (listaDeVertices[u] == null) {
+                listaDeVertices[u] = vertice1             // se agregan los dos vertices a la lista de vertices
+            }
+            if (listaDeVertices[v] == null) {             // si estos ya se encuentran en la lista de vectores el programa continua y no los agrega
+                listaDeVertices[v] = vertice2
+            }
+
+            j = listaDeVertices.indexOf(vertice1)              // se buscan los vertices y se aumentan sus respectivos grados interiores y exteriores
+            listaDeVertices[j]!!.gradoExterior +=1
+            j = listaDeVertices.indexOf(vertice2)
+            listaDeVertices[j]!!.gradoInterior +=1
+            i++
+        }
     }
 
     /* Agrega un lado al digrafo. Si el lado a agregar contiene
@@ -29,6 +93,7 @@ public class GrafoDirigidoCosto : Grafo {
      está en el grafo, sin importar que tengan diferentes costos.
      */
     fun agregarArcoCosto(a: ArcoCosto) : Boolean {
+
     }
 
     // Retorna el grado del grafo. Si el vértice no pertenece al grafo se lanza una RuntimeException
@@ -45,10 +110,12 @@ public class GrafoDirigidoCosto : Grafo {
 
     // Retorna el número de lados del grafo
     override fun obtenerNumeroDeLados() : Int {
+        return numDeLados
     }
 
     // Retorna el número de vértices del grafo
     override fun obtenerNumeroDeVertices() : Int {
+        return numDeLados
     }
 
     /* 
