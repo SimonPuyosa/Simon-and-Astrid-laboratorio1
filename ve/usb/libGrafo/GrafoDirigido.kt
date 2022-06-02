ff540d46da4b4c22b48e6535d517deb11614f9ca
Simon <18-10717@gmail.com>
2022-06-02 00:49:37 -0400

    eliminacion de clases innecesarias y empieza documentacion final

--- ve/usb/libGrafo/GrafoDirigido.kt
+++ ve/usb/libGrafo/GrafoDirigido.kt
@@ -5,63 +5,66 @@ import javax.management.openmbean.KeyAlreadyExistsException
 
 
 public class GrafoDirigido : Grafo {
-    // atributos agregados por nosotros
+    // Propiedades del grafo dirigido
     var listaDeAdyacencia: Array<LinkedList<Int>?> = arrayOf(LinkedList())       // Arreglo, que puede ser nulo, de elementos que puden ser listas enlazadas o nulos
-    var numeroDeLados: Int = 0
-    lateinit var listaDeVertices: Array<Int?>                 // lista enlazada la cual contiene todos los vertices sin repeticiones
-    var numeDeVertices = 0
+    var numDeLados: Int = 0
+    lateinit var listaDeVertices: Array<Int?>                 // arreglo que contiene todos los vertices sin repeticiones
+    var numDeVertices = 0
 
-
-    // Se construye un grafo a partir del número de vértices
+    /** Contruye un grafo dirigido partiendo de una integral que representaa el numero de vertices
+     */
     constructor(numDeVertices: Int) {
-        // Entrada: un entero que determina el numero de vertices que tendra el grafo
+        /** Entrada: un entero que determina el numero de vertices que tendra el grafo
+         *  Precondicion: numDeVertices > 0
+         *  Postcondicion: listaDeAdyacencia.size() == numDeVertices
+         *  Tiempo de operacion: O(|V|)
+         */
+
         listaDeAdyacencia = arrayOfNulls(numDeVertices)
     }
 
-
-
-    /** Se construye un grafo a partir de un archivo. El formato es como sigue.
-     *  La primera línea es el número de vértices.
-     *  La segunda línea es el número de lados. Las siguientes líneas
-     *  corresponden a los lados, con los vértices de un lado separados por un espacio en blanco.
-     *  Se asume que los datos del archivo están correctos, no se verifican.
+    /** Constructor que dado un String de la ubicacion de un archivo, lee el archivo y segun sus datos genera un grafo
+     *  Este grafo estara representado por una lista de adyacencia la cual sera un arreglo de |V| elementos en donde
+     *  cada casilla es una linked list de tipo entero o nulo, por lo que los vertices seran representados como enteros
      */
     constructor(nombreArchivo: String) {
         /** Entrada: un string de la ubicacion del archivo a buscar
+         *  Precondicion: el string debe tener una ubicacion real y valida del archivo
+         *  Postcondicion:
+         *      numDeVertice = Archivo[0]
+         *      numDeLados = Archivo[1]
+         *      listaDeAdyacencia.sizes == listaDeVertices.size == numDeVertices
+         *  Tiempo de operacion: O(|V| + |E|)
          */
 
         val a = File(nombreArchivo).readLines()                 // Se guarda el archivo en la variable a de tipo List<String>
-        numeDeVertices = a[0].toInt()               // Se obtiene de la primera linea linea el numero de vertices
-        //if (ListaDeAdyacencia == null) { ListaDeAdyacencia = arrayOfNulls(numeDeVertices) }
-        listaDeAdyacencia = arrayOfNulls(numeDeVertices)        // Se genera la lista de adyacencia (array de listas enlazadas)
-        listaDeVertices = arrayOfNulls(numeDeVertices)                          // Se genera la lista de vertices la cual sera una lista enlazada
-        numeroDeLados = a[1].toInt()                // Se obtiene de la segunda linea el numero de lados
+        numDeVertices = a[0].toInt()                            // Se obtiene de la primera linea linea el numero de vertices
+        listaDeAdyacencia = arrayOfNulls(numDeVertices)         // Se genera la lista de adyacencia (array de listas enlazadas)
+        listaDeVertices = arrayOfNulls(numDeVertices)           // Se genera la lista de vertices la cual sera una lista enlazada
+        numDeLados = a[1].toInt()                               // Se obtiene de la segunda linea el numero de lados
 
-
+        // Agregamos los lados a la lista de adyacencia
         var i = 2
-        var u: Int
-        var v: Int
+        var vertice1: Int
+        var vertice2: Int
         var temp: List<String>
+        while (i < 2 + numDeLados && a[i] != ""){                // se itera por las demas lineas del archivo hasta que este se acabe
+            temp = a[i].split(" ").filter {it != ""}   // se separa cada linea por espacios
 
-
-        while (i < 2 + numeroDeLados && a[i] != ""){                                     // se itera por las demas lineas del archivo hasta que este se acabe
-            temp = a[i].split(" ").filter {it != ""}  // se separa cada linea por espacios
-
-            if (listaDeAdyacencia[temp[0].toInt()] == null){
+            if (listaDeAdyacencia[temp[0].toInt()] == null){     // si el elemento del arreglo es nulo se le asigna un linked list vacio
                 listaDeAdyacencia[temp[0].toInt()] = LinkedList<Int>()
             }
 
-            v = temp[1].toInt()
-            u = temp[0].toInt()
-            if (listaDeAdyacencia[temp[0].toInt()]!!.indexOf(v) != -1) throw KeyAlreadyExistsException("el objeto esta repetido")
-            listaDeAdyacencia[temp[0].toInt()]!!.addFirst(v)       // se agrega el vertice final, en el arreglo especificamente en el inicio
-            if (listaDeVertices[v] == null) {
-                listaDeVertices[v] = v     // se agregan los dos vertices a la lista de vertices
+            vertice2 = temp[1].toInt()
+            vertice1 = temp[0].toInt()
+            if (listaDeAdyacencia[vertice1]!!.indexOf(vertice2) != -1) throw KeyAlreadyExistsException("el objeto esta repetido")
+            listaDeAdyacencia[temp[vertice1].toInt()]!!.addFirst(vertice2)       // se agrega el vertice 2, al inicio del arreglo
+            if (listaDeVertices[vertice1] == null) {
+                listaDeVertices[vertice1] = vertice1             // se agregan los dos vertices a la lista de vertices
             }
-            if (listaDeVertices[u] == null) {  // si estos ya se encuentran en la lista el programa continua y no los agrega
-                listaDeVertices[u] = u
+            if (listaDeVertices[vertice2] == null) {             // si estos ya se encuentran en la lista de vectores el programa continua y no los agrega
+                listaDeVertices[vertice2] = vertice2
             }
-
             i++
         }
     }
@@ -82,7 +85,7 @@ public class GrafoDirigido : Grafo {
             throw RuntimeException("El lado a agregar contiene un vertice que no pertenece al grafo")
         }
         if (listaDeAdyacencia[a.inicio]!!.indexOf(a.fin) == -1){   // se verifica que el lado no exista
-            this.numeroDeLados += 1
+            this.numDeLados += 1
             listaDeAdyacencia[a.inicio]!!.add(a.fin)
             return true
         }
@@ -121,7 +124,7 @@ public class GrafoDirigido : Grafo {
         var grados = 0
         var actual: LinkedList<Int>?
 
-        while (i < numeDeVertices){
+        while (i < numDeVertices){
             if (listaDeAdyacencia[i] != null){
                 j = 0
                 actual = listaDeAdyacencia[i]
@@ -140,14 +143,36 @@ public class GrafoDirigido : Grafo {
     override fun obtenerNumeroDeLados() : Int {
         /**  Salida: una integral del numero de lados del grafo
          */
-        return numeroDeLados
+        return numDeLados
     }
 
     // Retorna el número de vértices del grafo
     override fun obtenerNumeroDeVertices() : Int {
         /**  Salida: una integral del numero de vertices del grafo
          */
-        return numeDeVertices
+        return numDeVertices
+    }
+
+    /**
+     *  clase que dada una tabla de hash retorna un iterador
+     */
+    class AdyacenIterato(G: GrafoDirigido, private val v: Int) : Iterator<Arco> {
+        /** Entrada: un hashtable con el contenido a iterar
+         *  Salida: una iterador que retorna el conjunto de pares de claves y valores
+         */
+        private val adyacentes = G.listaDeAdyacencia[v]!!
+        var i = 0
+
+        override fun hasNext(): Boolean {
+            if (adyacentes.size <= i) return false
+            return true
+        }
+
+        override fun next(): Arco {
+            val result = Arco(v, adyacentes[i])
+            i += 1
+            return result
+        }
     }
 
     /** Clase interna que sobreescribe el metodo iterator y lo iguala a AdyacenIterato
@@ -172,6 +197,42 @@ public class GrafoDirigido : Grafo {
         if (listaDeVertices[v] == null) throw RuntimeException("no se encuentra el vertice en el grafo")
         return AdyacenIterable(this, v)
     }
+
+
+    /**
+     *  clase que dada una tabla de hash retorna un iterador
+     */
+    private inner class LadAdyacenIterato(private val G: GrafoDirigido, private val v: Arco) : Iterator<Arco> {
+        /** Entrada: un hashtable con el contenido a iterar
+         *  Salida: una iterador que retorna el conjunto de pares de claves y valores
+         */
+        private var temp: Array<LinkedList<Int>?> = G.listaDeAdyacencia
+        private var actual: LinkedList<Int>? = null
+        private var i = 0
+        private var j: Int = 0
+
+        override fun hasNext(): Boolean {
+            while (i < G.numDeVertices){
+                if (temp[i] != null){
+                    j = 0
+                    actual = temp[i]
+                    while(actual != null && j < actual!!.size){
+                        if (actual!![j] == v.inicio) return true
+                        j += 1
+                    }
+                }
+                i++
+            }
+            return false
+        }
+
+        override fun next(): Arco {
+            val result = Arco(i, v.inicio)
+            i += 1
+            return result
+        }
+    }
+
 
     /** Clase interna que sobreescribe el metodo iterator y lo iguala a LadAdyacenIterato
      */
@@ -202,7 +263,40 @@ public class GrafoDirigido : Grafo {
 
         return LadAdyacenIterable(this, l)
     }
-    
+
+    /**
+     *  clase que dada una tabla de hash retorna un iterador
+     */
+    class LadosIterato(private val G: GrafoDirigido) : Iterator<Arco> {
+        /** Entrada: un hashtable con el contenido a iterar
+         *  Salida: una iterador que retorna el conjunto de pares de claves y valores
+         */
+        private val temp: Array<LinkedList<Int>?> = G.listaDeAdyacencia
+        private var actual: LinkedList<Int>? = null
+        private var i = 0
+        private var j: Int = 0
+
+        override fun hasNext(): Boolean {
+            while (i < G.numDeVertices){
+                if (temp[i] != null){
+                    actual = temp[i]
+                    if (actual != null && j < actual!!.size){
+                        return true
+                    }
+                }
+                j = 0
+                i++
+            }
+            return false
+        }
+
+        override fun next(): Arco {
+            val result = Arco(i, actual!![j])
+            j += 1
+            return result
+        }
+    }
+
     // Retorna todos los lados del digrafo
     override operator fun iterator() : Iterator<Arco> = LadosIterato(this)
     /** Salida: un iterador de arcos que iterara sobre todos los lados del grafo
